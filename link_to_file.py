465ff6da8a9f09f1a7f1b97790ec74704a92d397
elPytel <jaroslav.korner1@gmail.com>
2024-08-19 10:15:12 +0200

    formating

--- link_to_file.py
+++ link_to_file.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 from download import *
@@ -8,12 +7,13 @@ DEBUG = True
 VERBOSE = True
 JSON_FILE = "files.json"
 
+
 class Link_to_file:
     def __init__(self, title, link, size):
         self.title = title
         self.link = link
         self.size = size
-    
+
     def download(self, download_folder="."):
         """
         Stáhne soubor z internetu a uloží jej do souboru.
@@ -23,54 +23,65 @@ class Link_to_file:
             raise ValueError(f"Directory {download_folder} does not exist!")
         if os.path.exists(file_path):
             raise ValueError(f"File {self.title} already exists.")
-        
+
         if VERBOSE:
             print(f"Downloading file: {self.title}")
-        
+
         response = download_page(self.link)
         save_binary_file(response, file_path)
-        
+
         if VERBOSE:
             print(f"File {self.title} was downloaded.")
-    
+
     def server_name(self):
         return self.link.split("/")[2]
-    
+
     def to_json(self):
         return json.dumps(self.__dict__)
-    
+
     def from_json(self, json_str):
         link_2_file = Link_to_file("", "", "")
         link_2_file.__dict__ = json.loads(json_str)
         return link_2_file
-    
+
     def colorize(self):
         return f"Title: {Blue}{self.title}{NC} \nLink: {Blue}{self.link}{NC} \nSize: {Blue}{self.size}{NC}"
 
     def __str__(self):
         return f"Title: {self.title} \nLink: {self.link} \nSize: {self.size}"
-    
+
     def __eq__(self, other):
-        return self.title == other.title and self.link == other.link and self.size == other.size
-    
-def load_links_from_file(file_path = JSON_FILE):
-    """ 
+        return (
+            self.title == other.title
+            and self.link == other.link
+            and self.size == other.size
+        )
+
+
+def load_links_from_file(file_path=JSON_FILE):
+    """
     load files from json
-    """ 
+    """
     link_2_files = []
     link_2_file = Link_to_file("", "", "")
-    with open(JSON_FILE, "r") as file:
+    with open(file_path, "r") as file:
         for line in file:
             if line == "":
                 continue
             link_2_file = link_2_file.from_json(line)
             link_2_files.append(link_2_file)
-    return link_2_files 
+    return link_2_files
 
-def save_links_to_file(link_2_files, file_path = JSON_FILE):
+
+def save_links_to_file(link_2_files, file_path=JSON_FILE, append=False):
     """
     save files to json
     """
-    with open(file_path, "w", encoding=ENCODING) as file:
+    if append:
+        mode = "a"
+    else:
+        mode = "w"
+
+    with open(file_path, mode, encoding=ENCODING) as file:
         for link_2_file in link_2_files:
             file.write(link_2_file.to_json() + "\n")
