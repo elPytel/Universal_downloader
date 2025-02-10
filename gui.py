91e53172a30608bb069df3496cb39b669fe09b40
elPytel <jaroslav.korner1@gmail.com>
2025-02-10 13:03:14 +0100

    Přidání clear tlačítka

--- gui.py
+++ gui.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from link_to_file import *
 from sdilej_downloader import Sdilej_downloader
 from main import download_folder, JSON_FILE
+import threading
 
 class DownloaderGUI(tk.Tk):
     def __init__(self):
@@ -29,17 +30,30 @@ class DownloaderGUI(tk.Tk):
         self.search_type_menu = ttk.OptionMenu(search_frame, self.search_type_var, "relevance", *Sdilej_downloader.search_types.keys())
         self.search_type_menu.pack(side=tk.LEFT, padx=5)
 
-        self.search_button = ttk.Button(search_frame, text="Search", command=self.search_files)
+        self.search_button = ttk.Button(search_frame, text="Search", command=self.start_search_thread)
         self.search_button.pack(side=tk.LEFT, padx=5)
 
-        self.download_button = ttk.Button(search_frame, text="Download Selected", command=self.download_selected)
+        action_frame = ttk.Frame(self)
+        action_frame.pack(pady=5, padx=5, fill=tk.X)
+
+        self.download_button = ttk.Button(action_frame, text="Download Selected", command=self.start_download_thread)
         self.download_button.pack(side=tk.LEFT, padx=5)
 
-        self.save_button = ttk.Button(search_frame, text="Save Selected", command=self.save_selected)
+        self.save_button = ttk.Button(action_frame, text="Save Selected", command=self.save_selected)
         self.save_button.pack(side=tk.LEFT, padx=5)
 
-        self.load_button = ttk.Button(search_frame, text="Load from file", command=self.load_selected)
+        self.load_button = ttk.Button(action_frame, text="Load from file", command=self.load_selected)
         self.load_button.pack(side=tk.LEFT, padx=5)
+
+        self.remove_successful_var = tk.BooleanVar()
+        self.remove_successful_check = ttk.Checkbutton(action_frame, text="Remove successful from json", variable=self.remove_successful_var)
+        self.remove_successful_check.pack(side=tk.LEFT, padx=5)
+
+        self.clear_button = ttk.Button(action_frame, text="Clear All", command=self.clear_all)
+        self.clear_button.pack(side=tk.LEFT, padx=5)
+
+        self.clear_not_selected_button = ttk.Button(action_frame, text="Clear Not Selected", command=self.clear_not_selected)
+        self.clear_not_selected_button.pack(side=tk.LEFT, padx=5)
 
         self.results_frame = ttk.Frame(self)
         self.results_frame.pack(pady=10, fill=tk.BOTH, expand=True)
@@ -80,6 +94,9 @@ class DownloaderGUI(tk.Tk):
         self.check_vars[index] = not self.check_vars[index]
         self.results_tree.item(item, values=(self.get_check_symbol(self.check_vars[index]), *self.results_tree.item(item)["values"][1:]))
 
+    def start_search_thread(self):
+        threading.Thread(target=self.search_files).start()
+
     def search_files(self):
         self.log("Search initiated...", "info")
         prompt = self.search_entry.get()
@@ -92,6 +109,9 @@ class DownloaderGUI(tk.Tk):
         self.check_vars = [False for _ in link_2_files]
         for i, link_2_file in enumerate(link_2_files):
             self.results_tree.insert("", "end", values=(self.get_check_symbol(False), link_2_file.title, link_2_file.size, link_2_file.link), tags=(str(i),))
+
+    def start_download_thread(self):
+        threading.Thread(target=self.download_selected).start()
 
     def download_selected(self):
         self.log("Download initiated...", "info")
@@ -118,12 +138,17 @@ class DownloaderGUI(tk.Tk):
             if (not compare_sizes(file_size, link_2_file.size, 20/100) and link_2_file.size != None) or (link_2_file.size == None and file_size < 1024):
                 self.log(f"File {link_2_file.title} was not downloaded correctly.", "error")
                 self.log(f"File size: {file_size} expected: {link_2_file.size}", "error")
-                if not DEBUG:
-                    os.remove(f"{download_folder}/{link_2_file.title}")
-                    self.log(f"File {link_2_file.title} was removed.", "info")
+                os.remove(f"{download_folder}/{link_2_file.title}")
+                self.log(f"File {link_2_file.title} was removed.", "info")
             else:
                 successfull_files.append(link_2_file)
                 self.log(f"File {link_2_file.title} of size {size_int_2_string(file_size)} was downloaded.", "success")
+        
+        self.log(f"Downloaded files: {len(successfull_files)}", "success")
+
+        if self.remove_successful_var.get():
+            self.log("Removing successful downloads from the list...", "info")
+            remove_links_from_file(successfull_files, JSON_FILE)
 
     def save_selected(self):
         self.log("Saving selected items...", "info")
@@ -143,6 +168,19 @@ class DownloaderGUI(tk.Tk):
         for i, link_2_file in enumerate(link_2_files):
             self.results_tree.insert("", "end", values=(self.get_check_symbol(False), link_2_file.title, link_2_file.size, link_2_file.link), tags=(str(i),))
         self.log(f"Loaded items: {len(link_2_files)}", "success")
+
+    def clear_all(self):
+        self.results_tree.delete(*self.results_tree.get_children())
+        self.check_vars = []
+        self.log("Cleared all displayed files.", "info")
+
+    def clear_not_selected(self):
+        items_to_keep = [(self.results_tree.item(item)["values"], self.results_tree.item(item)["tags"]) for i, item in enumerate(self.results_tree.get_children()) if self.check_vars[i]]
+        self.results_tree.delete(*self.results_tree.get_children())
+        self.check_vars = [True for _ in items_to_keep]
+        for values, tags in items_to_keep:
+            self.results_tree.insert("", "end", values=values, tags=tags)
+        self.log("Cleared not selected files.", "info")
 
     def log(self, message, tag="info"):
         self.log_text.config(state=tk.NORMAL)
