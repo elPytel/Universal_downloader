abba4a8ba2a9fe5d1e4ff3f0afcbcff935f7cb51
elPytel <jaroslav.korner1@gmail.com>
2025-10-12 12:10:40 +0200

    opravené testy (rozšíření cetsy)

--- tests/test_sdilej_downloader.py
+++ tests/test_sdilej_downloader.py
@@ -1,5 +1,9 @@
 import pytest
 
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from main import *
 from link_to_file import Link_to_file
 from sdilej_downloader import *
